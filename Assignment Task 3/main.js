84e1eaf5008d928748f3816a4f0bd929fcde2a82
Taha Saif <160765440+Tahasaif3@users.noreply.github.com>
2024-05-15 16:40:53 +0500

    Add files via upload

---
+++ Assignment Task 3/main.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+async function checkFriend() {
+    let friendName = await inquirer.prompt([
+        {
+            name: "Name",
+            type: "input",
+            message: "Enter your name:",
+        },
+    ]);
+    switch (friendName.Name) {
+        case "Taha":
+            console.log("Hello Taha! You are my friend.");
+            break;
+        case "Ashad":
+            console.log("Hello Ashad! You are my friend.");
+            break;
+        case "Ahmed":
+            console.log("Hello Ahmed! You are my friend.");
+            break;
+        case "Zakir":
+            console.log("Hello Zakir! You are my friend.");
+            break;
+        default:
+            console.log("You are not my friend.");
+            break;
+    }
+}
+checkFriend().catch(error => {
+    console.error('Error occurred:', error);
+});
